ad76a5cde57a9eef497f309fd84cb26aada9ae2e
feng.shen <feng.shen@nio.com>
2020-08-10 13:41:59 +0800

    项目结构目录提交

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Button from './Button.vue'
 let EnhancedElemUI = {}
 EnhancedElemUI.install = function (Vue, options) {
-  Vue.component(testPanel.name, Button) // testPanel.name 组件的name属性
+  Vue.component(Button.name, Button) // testPanel.name 组件的name属性
 }
 export default EnhancedElemUI
