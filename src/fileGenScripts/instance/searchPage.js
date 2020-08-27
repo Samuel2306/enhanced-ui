763080f8c154b55df108b296c61b62010d681fd0
feng.shen <feng.shen@nio.com>
2020-08-27 16:25:12 +0800

    nodejs动态生成vue文件功能提交

--- src/fileGenScripts/instance/searchPage.js
+++ src/fileGenScripts/instance/searchPage.js
@@ -1,41 +1,81 @@
 const path = require('path')
 let SearchPageGenerator = require(path.resolve(__dirname, '../generator/SearchPageGenerator.js'))
-new SearchPageGenerator({
-  routeName: "searchPage",
-  genTargetFolder: 'pages',
-  fileName: 'a.vue',
-  modules: ['vue', 'axios', "import { mapState, mapMutations, mapGetter, mapActions} from 'vuex'"],
-  data: {
-    name: "sf",
-    age: 28,
-    list: [
-      {name: "sf", age: 28},
-      {name: "sf1", age: 29},
+
+let pages = [
+  {
+    routeName: "searchPage",
+    genTargetFolder: 'pages',
+    fileName: 'a.vue',
+    modules: ['vue', 'axios', "import { mapState, mapMutations, mapGetter, mapActions} from 'vuex'"],
+    data: {
+      name: "sf",
+      age: 28,
+      list: [
+        {name: "sf", age: 28},
+        {name: "sf1", age: 29},
+      ],
+      target: null
+    },
+    props: {
+      name: Boolean,
+    },
+    methods: {
+      add(){
+        console.log(this.name)
+      },
+      multiply: function(){
+        console.log(this.name)
+      },
+      multiply1: () => {
+        console.log(this.name)
+      }
+    },
+    // 定义当前页面的关联组件
+    components: [
+      {name: 'ExtendedButton', path: '@/components/button/button'}
     ],
-    target: null
+    template: [
+      '<v-extended-button></v-extended-button>'
+    ].join("")
   },
-  props: {
-    name: Boolean,
-  },
-  methods: {
-    add(){
-      console.log(this.name)
+  {
+    routeName: "searchPage1",
+    genTargetFolder: 'pages',
+    fileName: 'b.vue',
+    modules: ['vue', 'axios', "import { mapState, mapMutations, mapGetter, mapActions} from 'vuex'"],
+    data: {
+      name: "sf",
+      age: 28,
+      list: [
+        {name: "sf", age: 28},
+        {name: "sf1", age: 29},
+      ],
+      target: null
     },
-    multiply: function(){
-      console.log(this.name)
+    props: {
+      name: Boolean,
     },
-    multiply1: () => {
-      console.log(this.name)
-    }
-  },
-  // 定义当前页面的关联组件
-  components: [
-    {
-      name: 'ExtendedButton',
-      path: '@/components/button/button'
-    }
-  ],
-  template: [
-    '<v-extended-button></v-extended-button>'
-  ].join("")
+    methods: {
+      add(){
+        console.log(this.name)
+      },
+      multiply: function(){
+        console.log(this.name)
+      },
+      multiply1: () => {
+        console.log(this.name)
+      }
+    },
+    // 定义当前页面的关联组件
+    components: [
+      {name: 'ExtendedButton', path: '@/components/button/button'}
+    ],
+    template: [
+      '<v-extended-button></v-extended-button>'
+    ].join("")
+  }
+]
+
+pages.forEach((page) => {
+  new SearchPageGenerator(page)
 })
